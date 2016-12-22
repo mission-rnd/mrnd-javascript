2140f73a0ba9397ddbfa7bc8c22beaa587b1e760
Abhijith Ravuri <souravabhijith@gmail.com>
2016-12-22 22:17:13 +0530

    removed multiple describes

--- spec/tests/JSIntroSpec.js
+++ spec/tests/JSIntroSpec.js
@@ -1,21 +1,19 @@
 
-describe("JSIntro Test Suite", function(){
-	var JSIntro = require("../../src/JSIntro");
 
-	describe("JSIntro-Sum", function(){
+var JSIntro = require("../../src/JSIntro");
 
-		it("sum of 2 and 3 is 5", function(){
+describe("JSIntro-Sum", function(){
 
-			var i = JSIntro.Sum(2,3);
-			expect(i).toEqual(5);
-		});
+	it("sum of 2 and 3 is 5", function(){
 
-		it("sum of -1 and 3 is 2", function(){
+		var i = JSIntro.Sum(2,3);
+		expect(i).toEqual(5);
+	});
 
-			var i = JSIntro.Sum(-1,3);
-			expect(i).toEqual(2);
-		});
+	it("sum of -1 and 3 is 2", function(){
 
+		var i = JSIntro.Sum(-1,3);
+		expect(i).toEqual(2);
 	});
 
 });
